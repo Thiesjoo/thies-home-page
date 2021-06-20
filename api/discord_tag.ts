cdc56838215154bfe4e0be5e6be658de29f5cd0d
Thies Nieborg <thies2003@gmail.com>
2021-06-20 16:45:50 +0200

    Moved to Typescript and implemented TAG

---
+++ api/discord_tag.ts
@@ -0,0 +1,33 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { Client, User } from "discord.js";
+const client = new Client();
+
+let user: User = null;
+
+export default function (req: VercelRequest, res: VercelResponse) {
+	client.on("ready", async () => {
+		console.log(`Bot has logged in as ${client.user.tag}!`);
+		user = await client.users.fetch("299983320815763456");
+		if (!user) {
+			return res.status(404).json({ ok: false, error: "User not found ):" });
+		}
+
+		return generateResponse(res);
+	});
+
+	if (user) {
+		console.log("User was cached!");
+		return generateResponse(res);
+	}
+
+	client.login(process.env.DISCORD_TOKEN);
+}
+
+function generateResponse(res: VercelResponse) {
+	res.json({
+		ok: true,
+		tag: user.tag,
+		avatar: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`,
+		name: user.username,
+	});
+}
--- api/test.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-  res.json({
-    ok: true,
-    body: req.body,
-    message: "Ghello!"
-  })
-}
