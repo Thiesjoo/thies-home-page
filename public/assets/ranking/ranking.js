1c25d46f046eeefb6c12e46df0e0bf5d0ce83809
Thies Nieborg <thies2003@gmail.com>
2021-06-20 22:44:54 +0200

    Added basic vue app, connected with guanthebot

--- public/assets/ranking/ranking.js
+++ public/assets/ranking/ranking.js
@@ -1,10 +1,22 @@
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!',
-    users: [{ name: "test" }, { name: "othertest" }],
+    users: [{ name: "Name", counter: 0 }, { name: "Name", counter: 0 }, { name: "Name", counter: 0 }, { name: "Name", counter: 0 }, { name: "Name", counter: 0 }, { name: "Name", counter: 0 }, { name: "Name", counter: 0 }],
     ranks: [
-      { text: "First", color: "" },
+      { text: "Eerste", color: "#7fd1fb" },
+      { text: "Tweede", color: "#fd6470" },
+      { text: "Derde", color: "#ffffff" },
+      { text: "Vierde", color: "#fddc32" },
+      { text: "Vijfde", color: "#a2ed56" },
+      { text: "Zesde", color: "#e25ae0" },
+      { text: "Zevende", color: "#9549c1" },
     ]
   }
-})
+});
+
+
+(async function () {
+  const resp = await fetch("api/mongo");
+  const data = await resp.json();
+  app.users = data.data.slice(0, 7);
+})();
