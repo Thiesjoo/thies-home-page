c88e3f441bc6e27e26bd6c6c6fc0db01959e736e
Thies Nieborg <thies2003@gmail.com>
2021-06-21 11:50:09 +0200

    Transforming

--- public/assets/index/index.js
+++ public/assets/index/index.js
@@ -1,66 +1,37 @@
-const TAG_SEARCH_VALUE = "discord_tag";
-const ID_SEARCH_VALUE = "discord_id";
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
 
-const GIT_COMMIT_SEARCH_VALUE = "github_sha"
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// `Vue.extend()`, or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+  {
+    path: '/about', component: getPage("about")
+  },
+  { path: '/ranking', component: getPage("ranking") }
+]
 
-window.addEventListener('DOMContentLoaded', async () => {
-  console.log("This is running")
-  getDiscordUser()
-  getGithubInfo()
-});
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+// 4. Create and mount the root instance.
+// Make sure to inject the router with the router option to make the
+// whole app router-aware.
+const app = new Vue({
+  router
+}).$mount('#app')
 
 
-async function getDiscordUser() {
-  try {
-    const fetchResp = await fetch("api/discord_tag")
-    const response = await fetchResp.json()
-    contentWithId(TAG_SEARCH_VALUE, response.tag)
-    titleWithId(ID_SEARCH_VALUE, response.id)
-    changeFavicon(response.avatar)
-  } catch (e) {
-    contentWithId(TAG_SEARCH_VALUE, "????")
-    titleWithId(ID_SEARCH_VALUE, "????")
+function getPage(name) {
+  return async () => {
+
+    const test = await fetch(name + ".html")
+    return { template: `<div> ${await test.text()} </div>` }
   }
-}
-
-async function getGithubInfo() {
-  try {
-    const fetchResp = await fetch("api/github_data")
-    const response = await fetchResp.json()
-    contentWithId(GIT_COMMIT_SEARCH_VALUE, response.sha.slice(0, 7))
-    titleWithId(GIT_COMMIT_SEARCH_VALUE, response.sha)
-  } catch (e) {
-    contentWithId(GIT_COMMIT_SEARCH_VALUE, "????")
-    titleWithId(GIT_COMMIT_SEARCH_VALUE, "????")
-  }
-}
-
-
-/**
- * Replace innerhtml with content. On the element with ID=id
- */
-function contentWithId(id, content) {
-  document.getElementById(id).innerHTML = content
-}
-
-/** Replace the title of an element with a new title */
-function titleWithId(id, title) {
-  document.getElementById(id).title = title
-}
-
-/**
- * Change the favoicon to a PNG
- * @param {*} src The link to the PNG image
- */
-function changeFavicon(src) {
-  var link = document.createElement('link'),
-    oldLink = document.getElementById('dynamic-favicon');
-  link.id = 'dynamic-favicon';
-  link.rel = 'shortcut icon';
-  link.href = src;
-  link.type = "image/png"
-  if (oldLink) {
-    document.head.removeChild(oldLink);
-  }
-  document.head.appendChild(link);
 }
