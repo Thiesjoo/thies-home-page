ce107e77bab7c02cfb407809afeca52e620ea881
Thies Nieborg <37243142+Thiesjoo@users.noreply.github.com>
2022-03-22 11:02:52 +0100

    Added timer and background

--- src/views/Dash.vue
+++ src/views/Dash.vue
@@ -1,18 +1,99 @@
-<template>Hoi</template>
+<template>
+  <div class="background"></div>
+
+  <div class="centered">
+    <div class="info">
+      <h2 class="time">{{ time }}</h2>
+      <h3 class="greeting">Good morning, Thies.</h3>
+    </div>
+  </div>
+</template>
 
 <script>
 import { defineComponent } from "@vue/runtime-core";
 
-export default defineComponent({
+function getCurrentTime() {
+  return Intl.DateTimeFormat('nl-NL', {
+    hour: 'numeric',
+    minute: 'numeric',
+  }).format()
+}
 
+export default defineComponent({
+  data() {
+    return {
+      interval: null,
+      time: getCurrentTime()
+    }
+  },
+  beforeDestroy() {
+    clearInterval(this.interval)
+  },
+  created() {
+    this.interval = setInterval(() => {
+      this.time = getCurrentTime()
+    }, 1000)
+  }
 });
 </script>
 
 <style>
-/* Momentum albums: https://www.flickr.com/photos/152977080@N03/albums/72157681812020976 */
 body {
-  background: url("//live.staticflickr.com/2879/33538507176_f373036ca0_k.jpg");
-  background-repeat: no-repeat;
+  font-family: -apple-system, BlinkMacSystemFont, "Neue Haas Grotesk Text Pro",
+    "Helvetica Neue", Helvetica, Arial, sans-serif !important;
+  text-shadow: 0 1px 5px rgb(0 0 0 / 10%);
+  color: #fff;
+}
+
+/* Momentum albums: https://www.flickr.com/photos/152977080@N03/albums/72157681812020976 */
+.background {
+  background: url("https://source.unsplash.com/random/1920x1080/?landscape");
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: transparent;
   background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+}
+
+.centered {
+  height: 100vh;
+}
+
+.info {
+  flex-direction: column;
+}
+
+.centered,
+.info {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.time {
+  font-size: 1050%;
+  font-weight: 500;
+  letter-spacing: -5px;
+}
+
+.greeting {
+  font-size: 3.375rem;
+  font-weight: 300;
+}
+
+.time,
+.greeting {
+  cursor: default;
+  text-align: center;
+  line-height: 1;
+  padding: 0;
+  margin: 0;
 }
 </style>
