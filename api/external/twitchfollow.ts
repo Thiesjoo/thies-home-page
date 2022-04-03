098eff5a9bb68248cc5532372d6ff5d39ddffc8f
Thies Nieborg <37243142+Thiesjoo@users.noreply.github.com>
2022-04-03 22:59:17 +0200

    Added live follower list

--- api/external/twitchfollow.ts
+++ api/external/twitchfollow.ts
@@ -7,7 +7,8 @@ export default async function (req: VercelRequest, res: VercelResponse) {
 	try {
 		const result = await getProviderCreds(req, "twitch");
 
-		const twitch = await axios({
+		// TODO: This only works for < 100 live channels
+		const liveUsersRequest = await axios({
 			url: "https://api.twitch.tv/helix/streams/followed",
 			params: {
 				user_id: result.id,
@@ -18,8 +19,32 @@ export default async function (req: VercelRequest, res: VercelResponse) {
 			},
 		});
 
-		res.json({ ok: true, data: twitch.data.data });
+		const users = liveUsersRequest.data.data;
+
+		const profileImageRequest = await axios({
+			url: "https://api.twitch.tv/helix/users",
+			params: {
+				id: users.map((x) => x.user_id),
+			},
+			headers: {
+				"Client-Id": result.clientid,
+				Authorization: "Bearer " + result.accessToken,
+			},
+		});
+		const profileImages = profileImageRequest.data.data;
+
+		// Joining the profile images with correct users
+		const joined = users.map((x) => {
+			return {
+				...x,
+				profile_image_url: profileImages.find((y) => y.id === x.user_id)
+					?.profile_image_url,
+			};
+		});
+
+		res.json({ ok: true, data: joined });
 	} catch (e) {
+		console.log(e);
 		res.statusCode = 500;
 		res.json({ ok: false, error: e?.message });
 	}
--- src/widgets/TwitchFollow.vue
+++ src/widgets/TwitchFollow.vue
@@ -3,19 +3,38 @@
     <Base color="purple" :val="getFollows" @mouseover="open = true">
       <font-awesome-icon :icon="['fab', 'twitch']" />
     </Base>
-    <!-- Dropdown menu -->
-    <Transition>
+    <Transition
+      enter-active-class="transition ease-out duration-200"
+      enter-class="transform opacity-0 scale-95"
+      enter-to-class="transform opacity-100 scale-100"
+      leave-active-class="transition ease-in duration-175"
+      leave-class="transform opacity-100 scale-100"
+      leave-to-class="transform opacity-0 scale-95"
+    >
       <div
         v-if="open"
         @mouseover="open = true"
-        class="absolute right-0 w-48 py-2 mt-2 bg-white bg-gray-100 rounded-md shadow-xl"
+        class="absolute flex flex-col items-center right-0 w-64 py-2 mt-2 bg-white bg-gray-100 rounded-md shadow-xl"
       >
-      
         <a
-          href="#"
-          class="block px-4 py-2 text-sm text-gray-300 text-gray-700 hover:bg-gray-400 hover:text-white"
+          :href="'https://twitch.tv/' + item.url"
+          rel="nofollow"
+          class="grow inline-flex w-full px-1 py-3 text-sm text-gray-300 text-gray-700 hover:bg-gray-400 hover:text-white"
           v-for="item in data"
-        >{{item.id}}</a>
+        >
+          <div class="px-4 w-36 items-center align-top">
+            <img :src="item.avatar" class="object-contain rounded-full" />
+          </div>
+
+          <div class="flex flex-col w-full break-words" :title="item.title">
+            <b>{{ item.name }}</b>
+            <i>{{ item.game_name }}</i>
+            <div class="inline-flex items-center">
+              <div class="w-3 h-3 rounded-full bg-red-600"></div>
+              <div class="ml-1">{{ item.viewers }}</div>
+            </div>
+          </div>
+        </a>
       </div>
     </Transition>
   </div>
@@ -36,25 +55,20 @@ export default defineComponent({
         throw new Error(fetchRes.statusText);
 
       const res = await fetchRes.json()
-      this.data = res.data
-      return res.data.length
+      this.data = res.data.map(x => {
+        return {
+          name: x.user_name,
+          url: x.user_login,
+          title: x.title,
+          game_name: x.game_name,
+          avatar: x.profile_image_url,
+          stream_image: x.thumbnail_url.replace("{width}", "1920").replace("{height}", "1080"),
+          viewers: x.viewer_count
+        }
+      }).sort((a, b) => b.viewers - a.viewers)
+      return `${res.data.length} streams`
     }
   },
   components: { Base }
 });
 </script>
-
-<style scoped>
-/* we will explain what these classes do next! */
-.v-enter-active {
-  transition: opacity 0.5s ease;
-}
-.v-leave-active {
-  transition: opacity 0.8s ease;
-}
-
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
-</style>
