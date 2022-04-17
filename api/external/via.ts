da3d20b6ef595ae8f723bfcf5b1a22039a82fc43
Thies Nieborg <git@thies.dev>
2022-04-17 17:05:09 +0200

    Dev test

--- api/external/via.ts
+++ api/external/via.ts
@@ -1,17 +1,18 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import "axios";
 import axios from "axios";
+import getProviderCredentials from "./_getcredentials";
 
 export default async function (req: VercelRequest, res: VercelResponse) {
+	const result = await getProviderCredentials(req, "via");
+
 	const fetchRes = await axios({
 		url: "https://pos.svia.nl/api/account/",
 		headers: {
-			authorization: "Bearer ",
+			authorization: "Bearer " + result.accessToken,
 		},
 		method: "GET",
 	});
-	// TODO: Figure out how to not spam via ^^
-	// const fetchRes = { data: { balance: 1950 } };
 
 	res.statusCode = 200;
 	res.setHeader("Content-Type", "application/json");
