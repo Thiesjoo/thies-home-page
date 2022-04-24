cf304aca7c98637b661b274693069b3af7ef868d
Thies Nieborg <git@thies.dev>
2022-04-24 16:30:22 +0200

    Chrome doesnt like 407 status code

--- api/external/via.ts
+++ api/external/via.ts
@@ -36,7 +36,7 @@ export default async function (req: VercelRequest, res: VercelResponse) {
 				});
 			} catch (e) {}
 		}
-		res.statusCode = 407;
+		res.statusCode = 401;
 		res.setHeader("Content-Type", "application/json");
 		res.json({ error: "POS Creds expired" });
 		return;
