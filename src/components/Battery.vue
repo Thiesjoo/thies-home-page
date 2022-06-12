ae600c2a323db998e346c90d54b37a25c063ebde
Thies Nieborg <git@thies.dev>
2022-06-12 21:04:30 +0200

    Fixed battery styling

--- src/components/Battery.vue
+++ src/components/Battery.vue
@@ -1,22 +1,13 @@
 <template>
-	<div class="w-24">
-		<div
-			class="shadow w-1/2 rounded border-2 border-gray-400 flex my-1 flex-row relative bg-gray-300"
-		>
+	<div class="min-w-[8rem]">
+		<div class="shadow rounded border-2 border-gray-400 flex my-1 flex-row relative bg-gray-300">
 			<div
 				class="cursor-default text-xs font-bold leading-none flex items-center justify-center m-1 py-2 text-center text-white"
 				:class="['bg-' + color + '-400']"
 				:style="{ width: `${percentage}%` }"
 			>
-				<div
-					class="absolute left-0 text-gray-700"
-					:class="{ 'mx-1': charging, 'mx-2.5': !charging }"
-				>
-					<font-awesome-icon
-						:icon="['fas', 'bolt']"
-						color="black"
-						v-if="charging"
-					/>
+				<div class="text-gray-700" :class="{ 'mx-1': charging, 'mx-2.5': !charging }">
+					<font-awesome-icon :icon="['fas', 'bolt']" color="black" v-if="charging" />
 					{{ percentage }}%
 				</div>
 			</div>
