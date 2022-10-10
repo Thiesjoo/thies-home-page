1dfb1abbddd837f6f47db35d2607346ad6fc9b28
Thies Nieborg <git@thies.dev>
2022-10-10 21:58:46 +0200

    lol committed token, already expired tho (;

--- src/components/widgets/RemoteDevices.vue
+++ src/components/widgets/RemoteDevices.vue
@@ -20,8 +20,7 @@ export default defineComponent({
 			const res = (
 				await axios.get("https://windows.thies.dev/output/devices/summary", {
 					headers: {
-						Authorization:
-							"Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MjQ1ZjhkMzdiM2M2MDRjZjAwMGU2OGQiLCJyb2wiOiJkZXZlbG9wZXIiLCJuYW0iOiJUaGllcyIsImlhdCI6MTY2NTQzMDM2MiwiZXhwIjoxNjY1NDMxMjYyLCJpc3MiOiJBdXRob3JpdW0tMCJ9.hJPnm7PqOGPQpNLRDaOxCQuL7oSGTF_opINkW0hX7-PPkcipl5NCBlSeGxNIuD3xFUxr-dAnRCnNfd9B21miyQB3XPXDAHmZwg16TEwPvabp0RxBnk2HLsUsgIRr1TOr-RIC6NC2JZtKOAGtQGlZygr16MjsEfsRfgeOyPuTAM0pTtpJ3XkCwx3UCY7Fr1QGehVCWys4Rau4ERTc9K68pZKJa9hdoi_z-bind5BftY7fXa7tsQREZfGN6C2JbVfQvL_0ehpBmSISC0lWkGa8VSwc0ksYBLQm7nAD7g7NImaaT8HWkonzZRtm-fQ-QLfgcmJFdRdGngFInAzBqC8ggw",
+						Authorization: "Bearer",
 					},
 				})
 			).data;
