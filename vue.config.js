697be68d5153926d2966cf1a4fad48543d4fb2e0
Thies Nieborg <thies.nieborg@student.uva.nl>
2022-10-23 12:58:52 +0200

    Op een schaal van 1-10, gaat dit sowieso 10 stuk

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,46 @@
 const EXTRA_KEYS = ["BASEURL"]
+const CspHtmlWebpackPlugin = require('csp-html-webpack-plugin');
+
 
 module.exports = {
+  configureWebpack: {
+    plugins: [new CspHtmlWebpackPlugin({
+      'script-src': ["'self'", "'https://static.cloudflareinsights.com/'"],
+      'style-src': ["'self'"],
+    }, {
+      enabled: process.env.NODE_ENV === 'production',
+    })],
+  },
   chainWebpack: config => {
+    // config.plugin("html").tap(([options]) => {
+    //   console.log('test', options)
+
+    //   return [{
+    //     ...options,
+    //     cspPlugin: {
+    //       enabled: true,
+    //       // enabled: modeArgument === 'production',
+    //       hashEnabled: {
+    //         'script-src': true,
+    //         'style-src': false,
+    //       },
+    //       nonceEnabled: {
+    //         'script-src': true,
+    //         'style-src': false,
+    //       },
+    //       policy: {
+    //         'base-uri': "'self'",
+    //         'default-src': "'self'",
+    //         'frame-src': "'none'",
+    //         'img-src': ["'self'", 'data:'],
+    //         'media-src': "'none'",
+    //         'object-src': "'none'",
+    //         'script-src': ["'self'"],
+    //         'style-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+    //       },
+    //     },
+    //   }]
+    // })
     config.plugin('define').tap(([options = {}]) => {
       return [{
         ...options, // these are the env variables from your .env file, if any arr defined
