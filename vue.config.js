8c57c2ff21470349d6cb94019b411b6313ee101b
Thies Nieborg <thies.nieborg@student.uva.nl>
2022-10-23 13:10:00 +0200

    Added more CSP domains

--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,8 @@ const CspHtmlWebpackPlugin = require('csp-html-webpack-plugin');
 module.exports = {
   configureWebpack: {
     plugins: [new CspHtmlWebpackPlugin({
-      'script-src': ["'self'", "'https://static.cloudflareinsights.com/'"],
-      'style-src': ["'self'"],
+      'script-src': ["'self'", "https://static.cloudflareinsights.com", "https://challenges.cloudflare.com", "https://www.google.com/"],
+      'style-src': ["'self'", "'unsafe-inline'", "ttps://fonts.googleapis.com"],
     }, {
       enabled: process.env.NODE_ENV === 'production',
     })],
