ca73f0bd28158e4d263f28a8b51d9c3e13d2dfc7
Thies <git@thies.dev>
2022-12-20 21:33:27 +0100

    Finished porting all modals

--- src/components/PasskeyManagerModal.vue
+++ src/components/PasskeyManagerModal.vue
@@ -1,61 +1,44 @@
 <template>
-	<div>
-		<a
-			class="text-white no-underline border border-transparent py-2 px-4 flex flex-row justify-center items-center rounded-md mx-2"
-			style="background-color: rgb(102, 96, 102)"
-			@click="open = true">
-			<font-awesome-icon :icon="[`fas`, `lock`]" class="mr-2" />
-			Manage Passkeys
-		</a>
-		<div>
-			<div
-				class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full flex justify-center items-center"
-				v-if="open"
-				v-click-outside="toggle">
-				<div class="relative p-8 w-full max-w-2xl h-full md:h-auto z-[100]">
-					<div class="relative rounded-lg shadow bg-gray-800">
-						<div class="flex justify-between items-start p-5 rounded-t border-b border-gray-600">
-							<h2 class="mt-6 text-center w-full text-3xl font-extrabold">Manage Passkeys</h2>
-							<button
-								type="button"
-								@click="toggle"
-								class="text-gray-400 bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-600 hover:text-white">
-								<font-awesome-icon :icon="['fas', 'xmark']" size="lg" />
-							</button>
-						</div>
-						<div class="pt-1 p-6 space-y-6 text-base leading-relaxed text-gray-400">
-							<div class="flex items-center flex-col space-y-2" v-if="!user.loading.userdata">
-								<div
-									v-for="passkey in passkeys"
-									class="bg-gray-900 rounded-md m-3 p-2 flex flex-row items-center text-lg w-[75%]">
-									<font-awesome-icon :icon="['fas', 'key']" class="mr-2" size="lg" />
-									<span class="font-bold text-base flex flex-row align-center items-center"
-										>{{ passkey.nickname }}
+	<Modal>
+		<template #button>
+			<a
+				class="text-white no-underline border border-transparent py-2 px-4 flex flex-row justify-center items-center rounded-md mx-2"
+				style="background-color: rgb(102, 96, 102)">
+				<font-awesome-icon :icon="[`fas`, `lock`]" class="mr-2" />
+				Manage Passkeys
+			</a>
+		</template>
 
-										<font-awesome-icon
-											:icon="['fas', 'edit']"
-											size="xs"
-											class="ml-2 text-gray-100 opacity-50 hover:opacity-100 hover:text-gray-200"
-											@click="() => editPasskey(passkey.id)" />
-									</span>
+		<template #title>Manage Passkeys</template>
 
-									<div class="ml-5 flex flex-col align-center w-[75%] font-light text-xs">
-										<span>Last used: {{ humanizeDuration(passkey.lastUsed) }} ago</span>
-										<span>Created: {{ humanizeDuration(passkey.createdAt) }} ago</span>
-										<span>Used {{ passkey.counter }} times</span>
-									</div>
+		<template #content
+			><div class="flex items-center flex-col space-y-2" v-if="!user.loading.userdata">
+				<div
+					v-for="passkey in passkeys"
+					class="bg-gray-900 rounded-md m-3 p-2 flex flex-row items-center text-lg w-[75%]">
+					<font-awesome-icon :icon="['fas', 'key']" class="mr-2" size="lg" />
+					<span class="font-bold text-base flex flex-row align-center items-center"
+						>{{ passkey.nickname }}
 
-									<div @click="() => deletePasskey(passkey.id)">
-										<font-awesome-icon :icon="['fas', 'trash']" class="ml-5 text-red-500" />
-									</div>
-								</div>
-							</div>
-						</div>
+						<font-awesome-icon
+							:icon="['fas', 'edit']"
+							size="xs"
+							class="ml-2 text-gray-100 opacity-50 hover:opacity-100 hover:text-gray-200"
+							@click="() => editPasskey(passkey.id)" />
+					</span>
+
+					<div class="ml-5 flex flex-col align-center w-[75%] font-light text-xs">
+						<span>Last used: {{ humanizeDuration(passkey.lastUsed) }} ago</span>
+						<span>Created: {{ humanizeDuration(passkey.createdAt) }} ago</span>
+						<span>Used {{ passkey.counter }} times</span>
 					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+
+					<div @click="() => deletePasskey(passkey.id)">
+						<font-awesome-icon :icon="['fas', 'trash']" class="ml-5 text-red-500" />
+					</div>
+				</div></div
+		></template>
+	</Modal>
 </template>
 <script lang="ts">
 import { useUserStore } from "@/store/user.store";
@@ -68,7 +51,6 @@ export default defineComponent({
 	data() {
 		return {
 			...window.env,
-			open: false,
 			passkeys: [] as {
 				nickname: string;
 				lastUsed: number;
@@ -79,9 +61,6 @@ export default defineComponent({
 		};
 	},
 	methods: {
-		toggle() {
-			this.open = !this.open;
-		},
 		async deletePasskey(id: string) {
 			console.warn("Deleting passkey with id: ", id);
 			const result = confirm(
