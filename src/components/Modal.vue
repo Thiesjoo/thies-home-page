afa9472cc22f4b247dc0a535b55a92b4888108df
Thies <git@thies.dev>
2022-12-20 21:59:44 +0100

    Some more changes

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -9,22 +9,27 @@
 					class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-100 w-[100vw] md:inset-0 h-modal h-[100vh] flex justify-center items-center"
 					v-if="open"
 					v-click-outside="toggle">
-					<div class="relative p-10 max-w-2xl h-auto">
+					<div class="relative p-10 h-auto">
 						<div
 							class="relative rounded-lg shadow bg-gray-700"
 							:style="{
 								backgroundColor,
 							}">
-							<div class="flex justify-between items-start p-5 rounded-t border-b border-gray-600">
-								<h2 class="mt-6 text-center w-full text-3xl font-extrabold text-white">
+							<div class="flex p-5 border-gray-600 justify-end flex-row items-center align-center">
+								<h2
+									class="mt-6 text-center w-full text-3xl font-extrabold text-white"
+									style="position: absolute; left: 50%; transform: translateX(-50%)">
 									<slot name="title"> </slot>
 								</h2>
 								<!-- FIXME: The text is not centered, because button is not in the correct place -->
 								<button
 									type="button"
 									@click="toggle"
-									class="text-gray-400 bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-600 hover:text-white">
-									<font-awesome-icon :icon="['fas', 'xmark']" size="lg" />
+									class="text-gray-400 bg-transparent text-sm p-1 pt-0 z-10">
+									<font-awesome-icon
+										:icon="['fas', 'xmark']"
+										size="lg"
+										class="rounded-full hover:bg-gray-600 hover:text-white p-2 h-5 w-5" />
 								</button>
 							</div>
 							<div class="pt-1 p-6 space-y-6 text-base leading-relaxed text-gray-400">
@@ -57,7 +62,7 @@ export default defineComponent({
 	},
 	computed: {
 		backgroundColor() {
-			return lightenDarkenColor("#374151", (window.openModals - 1) * -10);
+			return lightenDarkenColor("#374151", (window.openModals - 1) * -30);
 		},
 	},
 	methods: {
