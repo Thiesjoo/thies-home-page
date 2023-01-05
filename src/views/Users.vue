850c2acf14037a30e7e9c58ec2115e62742044de
Thies <git@thies.dev>
2023-01-05 14:59:25 +0100

    Changes with the new api

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,36 +1,26 @@
 <template>
 	<div class="container">
-		<h1>Chatter lijst voor {{ user }}</h1>
+		<h1>Chatter lijst voor {{ broadcaster }}</h1>
 		<p>
-			Eerste data van: {{ initTime }}. Laatst geupdate:
-			{{ updateTime }}
-		</p>
-		<p>
-			Alle chatters van de afgelopen stream (Laatste duizend berichten)!
-			<em>(This list has all chatters from the past stream (Last thousand messages)!)</em>
+			Deze data is verzameled op: {{ initTime }}. De lijsten zijn gereset op: {{ resetTime }}. De laatste update
+			was op: {{ updateTime }}
 		</p>
 		<div class="content">
 			<div class="twitchList">
-				<h2 style="font-weight: 800">TwitchList</h2>
+				<h2 style="font-weight: 800">Alle actieve chatters</h2>
 				<p>
-					<em>Alle actieve chatters</em>
+					<em>Van deze stream</em>
 				</p>
 				<ul>
-					<li style="list-style-type: none" v-for="(type, index) in types" :key="index">
-						<hr style="height: 20px" />
-						<h3 style="font-weight: 800">{{ userFriendlyTypes[type] }}</h3>
-						<ul>
-							<li v-for="(item, index1) in users[type]" :key="index1">
-								{{ item }}
-							</li>
-						</ul>
+					<li v-for="(item, index1) in allChatters" :key="index1" :class="item.type">
+						{{ item.name }}
 					</li>
 				</ul>
 			</div>
 			<div class="liveList">
-				<h2 style="font-weight: 800">LiveList</h2>
+				<h2 style="font-weight: 800">Alle mensen die in de viewerlijst hebben gestaan</h2>
 				<p>
-					<em> Alle chatters. Nieuwe gebruikers komen aan de onderkant van de lijst </em>
+					<em>Van deze stream. Elke 5 minuten ververst.</em>
 				</p>
 				<ul class="extraList">
 					<li v-for="(item, index1) in allUsers" :key="index1" :class="item.type">
@@ -43,7 +33,6 @@
 </template>
 
 <script lang="ts">
-import { filterArrayBasedOnType } from "@/helpers/filter";
 import { formatDate } from "@/helpers/formatDate";
 import { disconnect, getClient, initTMIClient, joinChannel } from "@/helpers/tmi";
 import { defineComponent } from "@vue/runtime-core";
@@ -57,93 +46,82 @@ type User = {
 };
 
 /** The frontend list doesn't really have to be in sync with backend, because lurking bots will not chat */
-const BOTLIST = [
-	"guanthebot",
-	"madestoutbot",
-	"cloudlog",
-	"discord_for_streamers",
-	"soundalerts",
-	"streamelements",
-	"streamlabs",
-	"carbot14xyz",
-	"commanderroot",
-	"tiddly",
-	"violets_tv",
-];
+const BOTLIST = ["guanthebot", "madestoutbot", "streamelements", "streamlabs"];
 
 const comp = defineComponent({
 	data(): {
-		users: {
-			mod: string[];
-			vip: string[];
-			user: string[];
-		};
+		allChatters: User[];
 		allUsers: User[];
-		user: string;
+		broadcaster: string;
 		initialTime: Date | null;
-		lastUpdatedTime: Date | null;
-		types: UserTypes[];
-		userFriendlyTypes: {
-			mod: string;
-			vip: string;
-			user: string;
-		};
+		lastMessageTime: Date | null;
+		lastListWipeTime: Date | null;
 	} {
 		return {
-			users: {
-				mod: [],
-				vip: [],
-				user: [],
-			},
+			allChatters: [],
 			allUsers: [],
+			broadcaster: "",
 			initialTime: null,
-			lastUpdatedTime: null,
-			user: "",
-			types: ["mod", "vip", "user"],
-			userFriendlyTypes: {
-				mod: "Mods 🍤",
-				vip: "Vips 🦐",
-				user: "Users 🥺",
-			},
+			lastMessageTime: null,
+			lastListWipeTime: null,
 		};
 	},
 	computed: {
 		initTime: function () {
-			//@ts-ignore
 			return this.initialTime !== null ? formatDate(this.initialTime) : "---";
 		},
 		updateTime: function () {
-			//@ts-ignore
-			return this.lastUpdatedTime !== null
-				? //@ts-ignore
-				  formatDate(this.lastUpdatedTime)
-				: "---";
+			return this.lastMessageTime !== null ? formatDate(this.lastMessageTime) : "---";
+		},
+		resetTime: function () {
+			return this.lastListWipeTime !== null ? formatDate(this.lastListWipeTime) : "---";
 		},
 	},
 	async mounted() {
 		const { user } = this.$route.query;
 		const parsedUser = (Array.isArray(user) ? user[0] : user) || "madestout";
-		this.user = parsedUser;
+		this.broadcaster = parsedUser;
 		pause.then(() => joinChannel(parsedUser));
 
-		const resp = await fetch(`api/twitch_users?user=${parsedUser}`);
-		const data = await resp.json();
-		if (!data.data) {
+		if (this.broadcaster !== "madestout") {
+			alert(
+				"Sorry, maar deze pagina is alleen beschikbaar voor Madestout. Live chatters zijn wel beschikbaar, maar historische data niet."
+			);
+			return;
+		}
+		const respViewers = await fetch(`https://twitchtracker.thies.dev/api/viewers?user=${parsedUser}`);
+		const viewers: {
+			ok: boolean;
+			data: User[];
+		} = await respViewers.json();
+
+		const respChatters = await fetch(`https://twitchtracker.thies.dev/api/chatters?user=${parsedUser}`);
+		const chatters: {
+			ok: boolean;
+			data: User[];
+			lastWipe: number;
+		} = await respChatters.json();
+
+		console.log(viewers, chatters);
+
+		if (!viewers.data || !chatters.data) {
+			alert("Stuk");
+			console.log("Geen data gevonden");
 			return;
 		}
 		this.initialTime = new Date();
-		this.lastUpdatedTime = new Date();
-		this.allUsers = data.data;
-		this.types.forEach((x) => {
-			this.users[x] = filterArrayBasedOnType(data.sorted, x);
-		});
+		this.lastMessageTime = new Date();
+		this.lastListWipeTime = new Date(chatters.lastWipe);
+
+		this.allUsers = viewers.data.sort((a, b) => a.name.localeCompare(b.name));
+		this.allChatters = chatters.data;
 	},
 	created() {
 		const client = getClient();
 		if (!client) return;
 
 		client.on("message", (channel, tags, message, self) => {
-			let name = tags["display-name"]?.toLowerCase() || tags.username || "";
+			let name = tags["display-name"] || tags.username || "";
 			let type: UserTypes = "user";
 			if (tags.mod) {
 				type = "mod";
@@ -152,14 +130,22 @@ const comp = defineComponent({
 			}
 
 			if (
-				!this.users[type].includes(name) && // No dupes
 				!name.startsWith("justinfan") && // Anon twitch user.
-				!BOTLIST.includes(name)
+				!BOTLIST.includes(name.toLowerCase())
 			) {
-				this.users[type].push(name);
-				this.users[type].sort();
-				this.allUsers.push({ name, type });
-				this.lastUpdatedTime = new Date();
+				this.lastMessageTime = new Date();
+
+				const index = this.allUsers.findIndex((user) => user.name.toLowerCase() === name.toLowerCase());
+				if (index === -1) {
+					this.allUsers.push({ name, type });
+
+					this.allUsers.sort((a, b) => a.name.localeCompare(b.name));
+				}
+
+				const index2 = this.allChatters.findIndex((user) => user.name.toLowerCase() === name.toLowerCase());
+				if (index2 === -1) {
+					this.allChatters.push({ name, type });
+				}
 			}
 		});
 	},
@@ -196,17 +182,17 @@ export default comp;
 	max-width: 500px;
 }
 
-.extraList {
+ul {
 	margin-top: 1em;
 	list-style: none; /* Remove default bullets */
 }
 
-.extraList li::before {
+ul li::before {
 	content: "\2022"; /* Add content: \2022 is the CSS Code/unicode for a bullet */
 	font-weight: bolder;
 	display: inline-block;
-	width: 1em;
-	margin-left: -1em;
+	width: 1.5em;
+	margin-left: -1, 5em;
 }
 
 .mod::before {
