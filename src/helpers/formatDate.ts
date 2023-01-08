f1f1e21cb0e21003fbe8851b38fcfcb9ad076024
Thies <37243142+Thiesjoo@users.noreply.github.com>
2023-01-08 18:44:23 +0100

    Twitch viewer list changes to new API (#22)
    * Changes with the new api
    
    * Oopsie style

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -3,6 +3,8 @@ import { format } from "date-fns";
 
 export function formatDate(value: Date) {
 	if (value) {
+		console.log("Got here,", value);
 		return format(value, "HH:mm:ss", { locale: nl });
 	}
+	return "";
 }
