6543418ab54ed69a98f562e8525b6fa5f2ca8c11
Thies <git@thies.dev>
2023-02-04 16:13:39 +0100

    CSP + Sentry with vite

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,42 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import checker from 'vite-plugin-checker'
+import { ViteCspPlugin } from 'vite-plugin-csp';
+import replace from '@rollup/plugin-replace';
 
 // https://vitejs.dev/config/
 const path = require("path");
 export default defineConfig({
+    treeshake: "smallest",
     plugins: [
+        replace({
+            __SENTRY_DEBUG__: false,
+            preventAssignment: true
+        },),
         vue(),
-        // checker({
-        //     typescript: true,
-        // }),
+        checker({
+            typescript: true,
+        }),
+        ViteCspPlugin({
+            'script-src': ["'self'", "https://challenges.cloudflare.com"],
+            'style-src': ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://fonts.gstatic.com"],
+            'object-src': ["'none'"],
+        }, {
+            inject: true,
+            enabled: process.env.VERCEL_ENV !== 'development',
+            hashEnabled: {
+                'style-src': false,
+                'script-src': true,
+            },
+            nonceEnabled: {
+                'style-src': false,
+                'script-src': true,
+            },
+        }),
     ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
     },
-    // root: "src",
-    // build: {
-    //     outDir: '../dist'
-    // }
 });
