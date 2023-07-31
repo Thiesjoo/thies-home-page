866d2b5527ae766d404235c3b07ba11d16c77c75
Thies <git@thies.dev>
2023-07-31 21:45:50 +0200

    User settings working!

--- src/helpers/oidc.ts
+++ src/helpers/oidc.ts
@@ -5,17 +5,27 @@ const userManager = new UserManager({
 	client_id: "thies-home-page",
 	redirect_uri: "http://localhost:3000/",
 	response_type: "code",
-	scope: "openid profile email settings goauthentik.io/api",
+	scope: "openid profile email settings",
 });
 
 export function popup() {
-	userManager.signinPopup().then((user) => {
+	userManager.signinPopup().then(async (user) => {
 		console.log("User signed in", user);
+		console.log(await userManager.getUser());
+		const access = user.access_token;
+		const resp = await fetch("https://authentik.thies.dev/application/o/userinfo/", {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${access}`,
+			},
+		});
+		const data = await resp.json();
+		console.log(data);
 	});
 }
 
 export function callback() {
 	userManager.signinPopupCallback().then((user) => {
-		console.log("User signed in", user);
+		console.log("CALLBACK: User signed in", user);
 	});
 }
